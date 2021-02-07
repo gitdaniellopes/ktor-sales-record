3b7a401eb19fec1672b37887ffe940fdf51c498e
Daniel <danieljfsantos@gmail.com>
2021-02-06 23:25:37 -0300

    Authenticating users

--- KtorSales/src/Application.kt
+++ KtorSales/src/Application.kt
@@ -1,8 +1,10 @@
 package daniel.lopes.co
 
+import daniel.lopes.co.data.checkPasswordForEmail
 import daniel.lopes.co.routes.loginRoute
 import daniel.lopes.co.routes.registerRoute
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.routing.*
@@ -14,15 +16,32 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
     install(DefaultHeaders)
     install(CallLogging)
-    install(Routing) {
-        registerRoute()
-        loginRoute()
-    }
     install(ContentNegotiation) {
         gson {
             setPrettyPrinting()
         }
     }
+    install(Authentication) {
+        configureAuth()
+    }
 
+    install(Routing) {
+        registerRoute()
+        loginRoute()
+    }
+}
+
+private fun Authentication.Configuration.configureAuth() {
+    basic {
+        realm = "Sales Server"
+        validate { credentials ->
+            val email = credentials.name
+            val password = credentials.password
+            if (checkPasswordForEmail(email, password)) {
+                //Um usuário autenticado com sucesso em nosso servidor
+                UserIdPrincipal(email)
+            } else null
+        }
+    }
 }
 
